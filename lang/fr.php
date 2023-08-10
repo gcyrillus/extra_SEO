6d7dad50381277d83d41225287c45cb041bfdd0b
gcyrillus <1832331+gcyrillus@users.noreply.github.com>
2023-08-10 09:49:37 +0200

    Update fr.php

--- lang/fr.php
+++ lang/fr.php
@@ -1,11 +1,11 @@
 <?php
     $LANG = array(
-    'L_PAGE_TITLE'				=> 'NewsLetter',
-    'L_PAGE_ADMIN_TITLE'		=> 'Administration du plugin "NewsLetter"',
+    'L_PAGE_TITLE'		=> 'NewsLetter',
+    'L_PAGE_ADMIN_TITLE'	=> 'Administration du plugin "NewsLetter"',
     'L_BACK_TO_ADMINISTRATION'  => 'Retour à l\'administration',
     
     # config.php
-    'L_CONFIG'					=> 'Page de Configuration',
+    'L_CONFIG'			=> 'Page de Configuration',
     'L_GOTO_ADMIN'              => 'Allez à la page administration',
     'L_ACTIVATE'                => 'Activer',
     'L_KEEP_NATIVE'             => 'Conserver impérativement le format natif? <br> exemple:<code style="background:silver">index.php?articleX/nom-article </code>',
@@ -13,20 +13,20 @@
 	Pour les personnaliser, vous pouvez installer le plugin :<a href="https://ressources.pluxml.org/datas/plugins/MyBetterUrls.zip">MyBetterUrls.zip</a>.',
     'L_EXCLUDE'                 => 'Exclure',
     'L_EXCLUDE_SELF_LINKS'      => 'Exclure les liens vers l\'article affiché (inclus lien de commentaires)',
-    'L_EXCLUDE_SITEMAP'			=>	'Exclure du sitemap (référencement)',
-    'L_EXCLUDE_SITEMAP_HELP'    =>'<p>Indiquer la liste des numéros de catégories et pages statiques à exclure du sitemap.</p>
+    'L_EXCLUDE_SITEMAP'		=> 'Exclure du sitemap (référencement)',
+    'L_EXCLUDE_SITEMAP_HELP'    => '<p>Indiquer la liste des numéros de catégories et pages statiques à exclure du sitemap.</p>
     <p>vous devez saisir les identifiants des catégories et pages statiques que vous souhaitez exclure séparé par un <b style="color:blue">|</b> .        
     <br>Par exemple : 001 <b style="color:blue">|</b> 003</p>
     <p>Lorsqu\'un article est présent dans une de ces catégories alors il n\'apparaitra pas dans le sitemap. Si il est présent dans plusieurs catégories et que l\'une d\'elle n\'est pas masqué alors l\'article sera visible dans le sitemap.</p>',
     'L_REMOVE_SITEMAP_CATEGORY' => 'Exclure catégories N°: ',
     'L_REMOVE_SITEMAP_STATIC'	=> 'Exclure page statiques N°: ',
-    'L_LIST_CATEGORY'			=> 'Liste des catégories actives',
-    'L_LIST_STATIC'				=> 'Liste des pages statiques actives',
+    'L_LIST_CATEGORY'		=> 'Liste des catégories actives',
+    'L_LIST_STATIC'		=> 'Liste des pages statiques actives',
     'L_PREVIEW_OPENSEARCH'      => 'Aperçu du fichier: opensearch.xml',
     'L_ADULT_CONTENT'           => 'Contenu pour Adulte',
-    'L_LANG_UNAVAILABLE'		=> 'Langue non disponible : %s',
-    'L_MAIN'					=> 'Général',
-    'L_SEND'                    => ' Envoyer',
+    'L_LANG_UNAVAILABLE'	=> 'Langue non disponible : %s',
+    'L_MAIN'			=> 'Général',
+    'L_SEND'                    => 'Envoyer',
     'L_COMPOSITION'             => 'Composition',
     'L_SITE_LOGO'               => 'Logo du site',
     'L_INSERT_LOGO'             => 'inserer le logo (modifiable en édition)',
@@ -35,7 +35,7 @@
     'L_ACCOUNT'                 => 'Compte:',
     'L_ADD_SAMEAS'              => 'Compte réseau social',
     'L_LIST_SAMEAS'             => 'reseau(x) enregistré(s)',
-    'L_SAVE'					=> 'Enregistrer',
+    'L_SAVE'			=> 'Enregistrer',
     
     # Admin
     'L_ADMIN'                   => 'Administration',
@@ -45,7 +45,7 @@
     'L_CONFIG_INFOS'            => 'Infos de configuration',
     'L_COMMIT_CHANGE'           => 'Valider les modifications',
     'L_CANCEL_ALL'              => 'Annuler tout',
-    'L_CANCEL'					=> ' ANNULER.',
+    'L_CANCEL'			=> ' ANNULER.',
  
     );
 ?>
